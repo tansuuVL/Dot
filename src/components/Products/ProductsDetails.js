a05973a01736089f940499d35279029dec384404
akai1881 <akaibegaliev1881@gmail.com>
2021-11-10 21:34:46 +0600

    add ProductsDetails component

---
+++ src/components/Products/ProductsDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductsDetails = () => {
+    return (
+        <div>
+            <h1>Products details</h1>
+        </div>
+    );
+};
+
+export default ProductsDetails;
